62bd032f345fc784777cc24aaecc32b731ed83c9
ktarrant <kmtarrant13@gmail.com>
2015-12-20 03:13:43 -0500

    Initial commit

---
+++ android/src/com/ktarrant/cloudWarfare/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ktarrant.cloudWarfare.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ktarrant.cloudWarfare.MainGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGdxGame(), config);
+	}
+}
---
+++ desktop/src/com/ktarrant/cloudWarfare/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ktarrant.cloudWarfare.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ktarrant.cloudWarfare.MainGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGdxGame(), config);
+	}
+}
